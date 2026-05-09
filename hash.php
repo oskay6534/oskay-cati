d71d16993eaee9391e8b2baecc5724f537300d44
oskay <mustafaoskay6534@gmail.com>
2026-05-09 16:22:35 +0300

    yeniler

--- hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Yeni şifre belirlemek için bu dosyayı tarayıcıda çalıştır
-$sifre = "osman1234"; 
-$hash = password_hash($sifre, PASSWORD_BCRYPT);
-
-echo "Aşağıdaki kodu .env dosyanızdaki ADMIN_PASSWORD_HASH kısmına yapıştırın:<br><br>";
-echo "<b>" . $hash . "</b>";
